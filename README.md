6ff3c4f1997ebffa3992403f34fcb657a6a8599e
Michael Bowen <mikee.bowen@gmail.com>
2015-01-25 12:29:35 -0800

    Make sorting loop that includes first and liast items in sort

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Sort Ski Resorts in the Western US by Altitude
+# Sort Ski Resorts in the Western US by Altitude Sorted with Selection Sort
+
+###http://www.nczonline.net/blog/2009/09/08/computer-science-in-javascript-selection-sort/
+
+###https://www.flickr.com/photos/edsuom/
+###http://stackoverflow.com/questions/14621656/how-to-make-line-break-in-ul-horizontal-list-for-every-x-list
--- js/additional.js
+++ js/additional.js
@@ -4,60 +4,75 @@ $(function(){
   function Resort(name, altitude, image){
   this.name   = name;
   this.altitude = altitude;
-  this.image = image;
+  this.image = image
   };
 
   var create = function(name, altitude, image) {
     var resort = new Resort(name, altitude, image);
     skiResorts.push(resort);
-    resort.domElement = $('<li class="mountains">' + resort.name + ' has an altitude of ' + resort.altitude + '</li>');
+    resort.domElement = $('<li class="mountains" data-altitude="' + resort.altitude + '"><img src="images/' + resort.image + '" />' + resort.name + '' + resort.altitude  + ' feet</li>');
     $("#resort-list").append(resort.domElement);
   };
 
   // create("Arapahoe Basin, Colorado" ,  13050 , "arapahoe-basin-colorado-small.jpg");
-  create("Loveland Ski Area, Colorado" , 13010);
-  create("Breckenridge, Colorado" ,  12998);
-  create("Telluride, Colorado" , 12570);
-  create("Snowmass, Colorado" ,  12510);
-  create("Keystone, Colorado" ,  12408);
-  create("Copper Mountain, Colorado" , 12313);
-  create("Crested Butte, Colorado" , 12162);
-  create("Santa Fe, New Mexico" ,  12075);
-  create("Winter Park, Colorado" , 12060);
-  create("Taos Ski Valley, New Mexico" , 11819);
-  create("Aspen Highlands, Colorado" , 11675);
-  create("Vail, Colorado" ,  11570);
-  create("Beaver Creek, Colorado" ,  11440);
-  create("Aspen, Colorado" , 11212);
-  create("Big Sky, Montana" ,  11166);
-  create("Moonlight Basin, Montana" ,  11166);
-  create("Mammoth Mountain, California" ,  11053);
-  create("Snowbird, Utah" ,  11000);
-  create("Brian Head, Utah" ,  10920);
+  // create("Aspen, Colorado" , 11212 , "aspen-colorado-small.jpg");
+  // create("Beaver Creek, Colorado" ,  11440, "beaver-creek-colorado-small.jpg");
+  // create("Big Sky, Montana" ,  11166 , "big--sky-montana-small.jpg");
+  // create("Breckenridge, Colorado" ,  12998 , "breckenridge-colorado-small.jpg");
+  // create("Brian Head, Utah" ,  10920 , "brian-head-utah-small.jpg");
+  // create("Copper Mountain, Colorado" , 12313 , "copper-mountain-colorado-small.jpg");
+  // create("Crested Butte, Colorado" , 12162 , "crested-butte-colorado-small.jpg");
+  // create("Keystone, Colorado" ,  12408 , "keystone-colorado-small.jpg");
+  // create("Loveland Ski Area, Colorado" , 13010 , "loveland-ski-area-colorado-small.jpg");
+  // create("Mammoth Mountain, California" ,  11053 , "mammoth-mountain-california-small.jpg");
+  // create("Moonlight Basin, Montana" ,  11166 , "moonlight-basin-montana-small.jpg");
+  // create("Santa Fe, New Mexico" ,  12075, "santa-fe-new-mexico-small.jpg");
+  // create("Snowbird, Utah" ,  11000 , "snowbird-utah-small.jpg");
+  // create("Snowmass, Colorado" ,  12510 , "snowmass-colorado-small.jpg");
+  // create("Telluride, Colorado" , 12570 , "telluride-colorado-small.jpg");
+  // create("Vail, Colorado" ,  11570 , "vail-colorado-small.jpg");
+  // create("Taos Ski Valley, New Mexico" , 11819 , "taos-ski-valley-new-mexico-small.jpg");
+  // create("Winter Park, Colorado" , 12060 , "winter-park-colorado-small.jpg");
 
-  $("#sortButton").on("click", function () {
+  create("Big Sky, Montana" ,  11166 , "big--sky-montana-small.jpg");
+  create("Arapahoe Basin, Colorado" ,  13050 , "arapahoe-basin-colorado-small.jpg");
+  create("Copper Mountain, Colorado" , 12313 , "copper-mountain-colorado-small.jpg");
+  create("Brian Head, Utah" ,  10920 , "brian-head-utah-small.jpg");
+  create("Breckenridge, Colorado" ,  12998 , "breckenridge-colorado-small.jpg");
+  create("Mammoth Mountain, California" ,  11053 , "mammoth-mountain-california-small.jpg");
+  create("Crested Butte, Colorado" , 12162 , "crested-butte-colorado-small.jpg");
+  create("Keystone, Colorado" ,  12408 , "keystone-colorado-small.jpg");
+  create("Santa Fe, New Mexico" ,  12075, "santa-fe-new-mexico-small.jpg");
+  create("Loveland Ski Area, Colorado" , 13010 , "loveland-ski-area-colorado-small.jpg");
+  create("Moonlight Basin, Montana" ,  11166 , "moonlight-basin-montana-small.jpg");
+  create("Vail, Colorado" ,  11570 , "vail-colorado-small.jpg");
+  create("Winter Park, Colorado" , 12060 , "winter-park-colorado-small.jpg");
+  create("Snowbird, Utah" ,  11000 , "snowbird-utah-small.jpg");
+  create("Snowmass, Colorado" ,  12510 , "snowmass-colorado-small.jpg");
+  create("Beaver Creek, Colorado" ,  11440, "beaver-creek-colorado-small.jpg");
+  create("Aspen, Colorado" , 11212 , "aspen-colorado-small.jpg");
+  create("Telluride, Colorado" , 12570 , "telluride-colorado-small.jpg");
+  create("Taos Ski Valley, New Mexico" , 11819 , "taos-ski-valley-new-mexico-small.jpg");
+
+  $("#sortButton").on("click", function sort() {
+    // alert(skiResorts.length);
     var c = 0;
-    var len = skiResorts.length
+    var len = $("li").length;
+    function addSorted() {
+      $current.prev().insertAfter($current);
+    }
 
-    for (var i = 0; i < len ; i++) {
-      min = i;
-      for (var j=i+1; j < len; j++){
-        if (skiResorts[j].altitude < skiResorts[min].altitude){
-          min = j;
-          skiResorts[j].domElement.removeClass("sorting");
-          skiResorts[min].domElement.addClass("sorting");
-        }
-      }
-      if (i != min){
-          // skiResorts[j].domElement.removeClass("sorting");
-          // skiResorts[i].domElement.addClass("sorting");
-
-          setTimeout( function (element) {
-            $("#resort-list").prepend(element);
-          }, c*500, skiResorts[i].domElement);
-          c++;
+    for (i = 0; i < len ; i++) {
+      // if (i != len) {
+      //   setTimeout(addSorted, 500);
+      // }
+      $current = $("li").eq(i);
+      while($current.data("altitude") < $current.prev().data("altitude")) {
+        addSorted();
+        $current.addClass("sorting");
+        $current.prev().removeClass("sorting");
       }
     }
+    $("li:last-child").append('<p>is the tallest ski resort in the US</p>');
   });
 });
-
