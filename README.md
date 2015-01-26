53a327d7d2ce2b144de59093f83fc3e969a9bd33
Michael Bowen <mikee.bowen@gmail.com>
2015-01-26 00:24:17 -0800

    Add CSS styling

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # Sort Ski Resorts in the Western US by Altitude Sorted with Selection Sort
 
+The problem for this assignment was to create a website that demonstrated how selection sort works by using it to sort ski resorts by altitude. I used a responsive grid that I downloaded from http://www.99lime.com/elements/ called [HTML Kickstart](http://www.99lime.com/elements/).
+
+The grid gave me the basic structure and I
+
+
 ###http://www.nczonline.net/blog/2009/09/08/computer-science-in-javascript-selection-sort/
 
 ###https://www.flickr.com/photos/edsuom/
--- js/additional.js
+++ js/additional.js
@@ -1,25 +1,24 @@
 $(function(){
   var skiResorts = [];
-
+  // Create Resort constructor
   function Resort(name, altitude, image){
   this.name   = name;
   this.altitude = altitude;
   this.image = image
   };
-
+  // Create variable to create resort objects and
   var create = function(name, altitude, image) {
     var resort = new Resort(name, altitude, image);
     skiResorts.push(resort);
-    resort.domElement = $('<li class="mountains" data-altitude="' + resort.altitude + '"><figure><img src="images/' + resort.image + '" /><figcaption>' + resort.name + ' ' + resort.altitude  + ' feet</li></figcaption></figure>');
+    resort.domElement = $('<li class="mountains" data-altitude="' + resort.altitude + '" style="background-image: url(images/' + resort.image + ');"><p>' + resort.name + ' ' + resort.altitude  + ' feet</p></li>');
     $("#resort-list").append(resort.domElement);
   };
-
+  // Create resorts
   create("Arapahoe Basin, Colorado" , 13050 , "arapahoe-basin-colorado-small.jpg");
   create("Aspen, Colorado" , 11212 , "aspen-colorado-small.jpg");
   create("Beaver Creek, Colorado" , 11440, "beaver-creek-colorado-small.jpg");
   create("Big Sky, Montana" , 11166 , "big--sky-montana-small.jpg");
   create("Breckenridge, Colorado" , 12998 , "breckenridge-colorado-small.jpg");
-  create("Brian Head, Utah" , 10920 , "brian-head-utah-small.jpg");
   create("Copper Mountain, Colorado" , 12313 , "copper-mountain-colorado-small.jpg");
   create("Crested Butte, Colorado" , 12162 , "crested-butte-colorado-small.jpg");
   create("Keystone, Colorado" , 12408 , "keystone-colorado-small.jpg");
@@ -33,22 +32,30 @@ $(function(){
   create("Vail, Colorado" , 11570 , "vail-colorado-small.jpg");
   create("Taos Ski Valley, New Mexico" , 11819 , "taos-ski-valley-new-mexico-small.jpg");
   create("Winter Park, Colorado" , 12060 , "winter-park-colorado-small.jpg");
-
-  $("#sortButton").on("click", function sort() {
+  // Create on click function to sort resorts
+  $("#sortButton").on("click", function() {
+    // create variable with length of list
     var len = $("li").length;
+    // Create function to insert lower resort after previous resort that was higher
     function addSorted() {
       $current.prev().insertAfter($current);
     }
-
+    // This for loop runs once for every item in the list
     for (i = 0; i < len ; i++) {
+      // Sets the value of $current to the current li
       $current = $("li").eq(i);
+      // While the value of the current li is smaller than the previous perform the function
       while($current.data("altitude") < $current.prev().data("altitude")) {
+        // call AddSorted function to insert small li after previous
         addSorted();
-        $current.addClass("sorting");
-        $current.prev().removeClass("sorting");
+        // below works, but selects too many <li>s
+        // $current.addClass("sorting");
+        // $current.prev().removeClass("sorting");
       }
-      setTimeout(addSorted, 1* 1000, $current);
+      // setTimeOut below, works, but makes the algorithm wrong
+      // setTimeOut(addSorted, 1* 1000, $current);
     }
-    $("li:last-child").append('<p>is the tallest ski resort in the US</p>');
+    // Add winning line to highest resort
+    $("li:last-child").append('<p class="winner">The tallest ski resort in the US</p>');
   });
 });
